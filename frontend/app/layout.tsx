dbb7a61b9ec89c72e06a0dd395c2ad1fd388a3f5
Rowan d'Auria <56480877+rowan-dauria@users.noreply.github.com>
2025-11-05 00:25:11 +0000

    remove unnecessary metadata component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from 'next'
 import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'Next.js App',
-  description: 'A barebones Next.js application',
-}
 
 export default function RootLayout({
   children,
