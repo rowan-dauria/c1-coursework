30d6c6bd27bb2de4fce261971704cc94e1665a09
Rowan d'Auria <56480877+rowan-dauria@users.noreply.github.com>
2025-11-19 17:32:57 +0000

    remove references to helloworld in package

--- fiveD_NN_package/fiveD_NN/__init__.py
+++ fiveD_NN_package/fiveD_NN/__init__.py
@@ -1,7 +1,6 @@
-from .hello_world import helloWorld
 from .neural_network import LightweightNN
 import version
 
 __version__ = version.version
 
-__all__ = ['helloWorld', 'LightweightNN', '__version__']
+__all__ = ['LightweightNN', '__version__']
