154370a0a0b161a0143ac3fd5171eb2a908cc015
Rowan d'Auria <56480877+rowan-dauria@users.noreply.github.com>
2025-11-19 15:14:27 +0000

    backend changes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import dummy_package_rd
 app = FastAPI()
 
 app.add_middleware(
@@ -18,3 +19,11 @@ app.add_middleware(
 @app.get("/api")
 async def hello_world():
     return {"message": "Hello from the backend world!"}
+
+@app.get("/dummy-package-version")
+async def version():
+    try:
+        return {"version": dummy_package_rd.__version__}
+    except:
+        return {"error": "Could not resolve version"}
+
