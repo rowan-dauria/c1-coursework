60e146dcf036a457a545951ef6e48666bc417269
Rowan d'Auria <56480877+rowan-dauria@users.noreply.github.com>
2025-11-19 20:56:35 +0000

    fix NN import

--- fiveD_NN_package/fiveD_NN/__init__.py
+++ fiveD_NN_package/fiveD_NN/__init__.py
@@ -1,6 +1,6 @@
 from .neural_network import LightweightNN
-import version
+from .version import version
 
-__version__ = version.version
+__version__ = version
 
 __all__ = ['LightweightNN', '__version__']
